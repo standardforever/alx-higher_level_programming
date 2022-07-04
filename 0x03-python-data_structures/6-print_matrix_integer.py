7f20538d468e132be6c87f9bcbfa00b2cb158be3
standardforever <standard.forever123@gmail.com>
2022-07-04 14:28:32 -0700

    updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if len(matrix) > 1:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if j == (len(matrix[i]) - 1):
