f923afd90cda0d674dcf8fd55a8ffe2b3ca75939
standardforever <standard.forever123@gmail.com>
2022-08-02 09:17:05 -0400

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+def read_file(filename="UTF8"):
 	"""It print the content of a file"""
-	with open(filename, 'r') as f:
+	with open(filename, 'r', encoding="") as f:
 		content = f.read()
 		print("{}".format(content))
+	
