f47ebb6d9663dba86fe58b9d6a5f111195237490
standardforever <standard.forever123@gmail.com>
2022-08-05 14:36:55 -0400

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """It reads a text file and print to stdout"""
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, encoding='UTF8') as f:
         file = f.read()
         print("{}".format(file))
