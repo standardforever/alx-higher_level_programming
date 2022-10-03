c95aeac19757b9cdb65a416a5893eaf0b739c8e2
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-10-03 18:45:48 -0400

    updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i;
-for (i = 0;  i < 3; i++) {
-  console.log(myArray);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
