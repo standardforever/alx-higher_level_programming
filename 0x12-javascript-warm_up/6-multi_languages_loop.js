a03e7f8d1aa34eb444756189600f670e85cd38e4
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-10-03 18:43:41 -0400

    updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0;  i < 3; i++) {
+let i;
+for (i = 0;  i < 3; i++) {
   console.log(myArray);
 }
