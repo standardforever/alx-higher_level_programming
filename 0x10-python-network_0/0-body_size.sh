0dbe4944456ada9c50a5a67ddd19c7ca4bab6a2a
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-10-23 07:42:46 -0400

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -si developersmindset.tech | grep Content-Length | tail -c 4
