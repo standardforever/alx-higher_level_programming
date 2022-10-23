4fbdc778db48bdb067e7c2f6b85a123b9175381a
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-10-23 09:05:43 -0400

    updated

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Curl display all HTTP methods the server will accept
-curl -IsLX OPTIONS $1 | grep allow | cut -d ":" -f2- | cut -d " " -f2-
+curl -IsLX "OPTIONS" $1 | grep "Allow" | cut -d ":" -f2- | cut -d " " -f2-
