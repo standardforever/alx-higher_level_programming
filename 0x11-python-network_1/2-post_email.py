28b44105bb4d7be99b2d98da8fa1e420f9fea6e2
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-10-25 03:19:43 -0400

    updated

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Sends a POST request with email
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data['email'] = sys.argv[2]
+    data = urllib.parse(data)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print(page.decode('utf-8'))
