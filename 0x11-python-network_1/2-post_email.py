19a7ee35c86f3cda508dcb75003eccf93d342c09
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-10-25 03:25:44 -0400

    updated

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,10 +9,10 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data['email'] = sys.argv[2]
+    data = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(data)
     data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
-        print(page.decode('utf-8'))
+        print(page.decode(encoding='utf-8'))
