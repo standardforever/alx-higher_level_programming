be7aaf1c63f28a3e60e98783aad046ef7673a5e9
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-10-25 03:38:10 -0400

    updated

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
             page = response.read()
             print(page.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e))
+        print("Error code: {}".format(e.code))
