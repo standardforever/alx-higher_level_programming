5d033d7c0a6d9202a49ea87f64be8cd745cea8a9
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-11-03 13:16:32 -0400

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-Prints different results of
-a request
-"""
-import urllib.request
+'''
+Makes a request and prints information about the body of the response
+'''
 
+from urllib.request import urlopen
 
-if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
+URL = 'https://alx-intranet.hbtn.io/status'
+
+if __name__ == '__main__':
+
+    with urlopen(URL) as r:
+        body = r.read()
         print("Body response:")
-        print("-\t type:", type(html))
-        print("-\t content:", html)
-        print("-\t utf8 content:", html.decode("utf-8"))
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode())
